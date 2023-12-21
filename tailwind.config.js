fa8e6815f8d8bfb6bfe6fb99c973610b7496aeb2
Foysal Rahman <fr25foysal@gmail.com>
2023-12-22 03:50:19 +0600

    set the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,29 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#5fad56",
+
+          secondary: "#4d9078",
+          
+          accent: "#f78154", 
+
+          white: "#ffffff",
+
+          black:"#000000",
+
+          neutral: "#f2c14e"
+        },
+      },
+      "dark",
+      "cupcake",
+    ]},
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
