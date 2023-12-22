06e46d806e3c2c83d7f99bc1058290662cbefb1d
Foysal Rahman <fr25foysal@gmail.com>
2023-12-22 19:59:23 +0600

    hosted

--- src/Hooks/usePublicAxios.jsx
+++ src/Hooks/usePublicAxios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const usePublicAxios = () => {
     const data = axios.create({
-        baseURL: 'http://localhost:5001',
+        baseURL: 'https://manageme-server.vercel.app',
       });
     return data
 };
--- src/Pages/Todos/Todos.jsx
+++ src/Pages/Todos/Todos.jsx
@@ -2,9 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import BoxContainer from '../../Components/Container/BoxContainer'
 import AddTask from '../../Pages/Todos/AddTask'
 import usePublicAxios from '../../Hooks/usePublicAxios';
+import useProvider from '../../Hooks/useProvider';
 
 const Todos = () => {
     const usePublcAxio = usePublicAxios()
+    const {user} = useProvider()
 
     const { data:todo_tasks ,isLoading} = useQuery({
         queryKey: ['todo'],
@@ -28,48 +30,69 @@ const Todos = () => {
     if (isLoading,taskload,dataload) {
         return "";
     }
-
+console.log(user);
     return (
       <div className="mt-5">
         <BoxContainer>
-          <AddTask></AddTask>
+          <div className="flex flex-col md:flex-row gap-5 ">
+            <div className="md:w-1/6 bg-gray-400 rounded-md p-5">
+              <div className="avatar">
+                <div className="w-24 rounded">
+                  <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
+              </div>
+            </div>
+            <div className="md:w-5/6">
+              <AddTask></AddTask>
+            </div>
+          </div>
+
           <div className="grid lg:grid-cols-3 gap-8 mt-5 ">
             <div className="bg-accent p-5 rounded-md min-h-64">
               <h2 className="bg-gray-100 rounded-md p-2 text-center font-medium text-lg">
                 To-dos
               </h2>
 
-
               {todo_tasks?.data?.map((task) => (
-                <div className='bg-gray-100 rounded-md p-2 mt-2 text-left font-medium ' key={task}>
-                  <div className='flex gap-5'>
+                <div
+                  className="bg-gray-100 rounded-md p-2 mt-2 text-left font-medium "
+                  key={task}
+                >
+                  <div className="flex gap-5">
                     {/* TODO: make the priority color responsive , if high another ,if low another */}
-                  <h2>{task.Title}</h2> <p className='text-[12px] bg-secondary px-2 py-[2px] rounded-md '>{task.Priority}</p>
+                    <h2>{task.Title}</h2>{" "}
+                    <p className="text-[12px] bg-secondary px-2 py-[2px] rounded-md ">
+                      {task.Priority}
+                    </p>
                   </div>
-                  
-                  <p className='text-sm'>{task.Description}</p>
-                  <p className='text-sm'>Deadline: {task.Deadline}</p>
+
+                  <p className="text-sm">{task.Description}</p>
+                  <p className="text-sm">Deadline: {task.Deadline}</p>
                 </div>
               ))}
-
             </div>
             <div className="bg-neutral p-5 rounded-md min-h-64">
               <h2 className="bg-gray-100 rounded-md p-2 text-center font-medium text-lg">
                 In progress
               </h2>
-                
+
               {inprogress_tasks?.data?.map((task) => (
-                <div className='bg-gray-100 rounded-md p-2 mt-2 text-left font-medium ' key={task}>
-                  <div className='flex gap-5'>
+                <div
+                  className="bg-gray-100 rounded-md p-2 mt-2 text-left font-medium "
+                  key={task}
+                >
+                  <div className="flex gap-5">
                     {/* TODO: make the priority color responsive , if high another ,if low another */}
-                  <h2>{task.Title}</h2> <p className='text-[12px] bg-secondary px-2 py-[2px] rounded-md '>{task.Priority}</p>
+                    <h2>{task.Title}</h2>{" "}
+                    <p className="text-[12px] bg-secondary px-2 py-[2px] rounded-md ">
+                      {task.Priority}
+                    </p>
                   </div>
-                  
-                  <p className='text-sm'>{task.Description}</p>
-                  <p className='text-sm'>Deadline: {task.Deadline}</p>
+
+                  <p className="text-sm">{task.Description}</p>
+                  <p className="text-sm">Deadline: {task.Deadline}</p>
                 </div>
               ))}
-
             </div>
             <div className="bg-secondary p-5 rounded-md min-h-64">
               <h2 className="bg-gray-100 rounded-md p-2 text-center font-medium text-lg">
@@ -77,17 +100,22 @@ const Todos = () => {
               </h2>
 
               {completed_tasks?.data?.map((task) => (
-                <div className='bg-gray-100 rounded-md p-2 mt-2 text-left font-medium ' key={task}>
-                  <div className='flex gap-5'>
+                <div
+                  className="bg-gray-100 rounded-md p-2 mt-2 text-left font-medium "
+                  key={task}
+                >
+                  <div className="flex gap-5">
                     {/* TODO: make the priority color responsive , if high another ,if low another */}
-                  <h2>{task.Title}</h2> <p className='text-[12px] bg-secondary px-2 py-[2px] rounded-md '>{task.Priority}</p>
+                    <h2>{task.Title}</h2>{" "}
+                    <p className="text-[12px] bg-secondary px-2 py-[2px] rounded-md ">
+                      {task.Priority}
+                    </p>
                   </div>
-                  
-                  <p className='text-sm'>{task.Description}</p>
-                  <p className='text-sm'>Deadline: {task.Deadline}</p>
+
+                  <p className="text-sm">{task.Description}</p>
+                  <p className="text-sm">Deadline: {task.Deadline}</p>
                 </div>
               ))}
-
             </div>
           </div>
         </BoxContainer>
